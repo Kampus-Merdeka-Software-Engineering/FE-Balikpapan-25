deb6f5a9894770671063140b2cbeab749f584302
Fiqri Juanesta <143619486+raymoreee@users.noreply.github.com>
2023-10-09 19:21:43 +0700

    Add files via upload
    FE Group m-25

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const form = document.getElementById("booking");
             const jam = document.getElementById("jam").value;
             const detail = document.getElementById("detail").value;
 
-            if (!nama || !email || !tanggal || !jam || !detail) {
+            if (!nama && !email && !tanggal && !jam && !detail) {
                 alert("Harap isi semua kolom yang telah disediakan");
             } else {
                 console.log("Nama: " + nama);
@@ -18,4 +18,38 @@ const form = document.getElementById("booking");
                 console.log("Jam: " + jam);
                 console.log("detail: " + detail);
             }
+        });
+        
+document.getElementById("booking").addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            // Ambil data dari input
+            var nama = document.getElementById("fname").value;
+            var email = document.getElementById("email").value;
+            var tanggal = document.getElementById("date").value;
+            var jam = document.getElementById("jam").value;
+            var detail = document.getElementById("detail").value;
+
+            // Buat baris baru dalam tabel
+            var newRow = document.getElementById("dataTable").getElementsByTagName("tbody")[0].insertRow();
+
+            // Masukkan data ke dalam sel-sel baru
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
+            var cell4 = newRow.insertCell(3);
+            var cell5 = newRow.insertCell(4);
+
+            cell1.innerHTML = nama;
+            cell2.innerHTML = email;
+            cell3.innerHTML = tanggal;
+            cell4.innerHTML = jam;
+            cell5.innerHTML = detail;
+
+            // Kosongkan input
+            document.getElementById("fname").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("date").value = "";
+            document.getElementById("jam").value = "";
+            document.getElementById("detail").value = "";
         });
